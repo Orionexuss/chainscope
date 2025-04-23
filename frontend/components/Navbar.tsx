6457f8420993a50a991c43ad182eda02c8eeee38
Orionexuss <sebastianvargaslopez36@gmail.com>
2025-04-23 15:38:21 -0500

    refactor: improve styling for Hero, Navbar, and Home components

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -3,16 +3,16 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="flex h-23 py-3 border-b border-gray">
+    <header className="flex bg-black items-center h-23 py-3 border-b border-gray">
       <Image
-        className="ml-2"
+        className="ml-5"
         src="/logo.png"
         width={230}
         height={190}
         alt="Logo"
       />
-      <nav className=" text-white flex font-(family-name:--abel) text-[22px] ml-auto mr-3 gap-5">
-        <button className="bg-transparent border w-28 h-12 cursor-pointer rounded-full">
+      <nav className=" text-white flex font-(family-name:--abel) text-[22px] ml-auto mr-5 gap-5">
+        <button className="bg-transparent border-2 w-28 h-12 cursor-pointer rounded-full">
           Login
         </button>
         <button className=" animate-background w-28 h-12 rounded-full bg-gradient-to-r from-[#00B594] via-[#4D63AC] to-[#8A009F] bg-[length:_400%_400%] p-1 [animation-duration:_6s] cursor-pointer">
