9542a794a5ab52f9bb77476250f77423aafbcaa8
Orionexuss <sebastianvargaslopez36@gmail.com>
2025-04-29 18:02:43 -0500

    feat: Add register form component

--- frontend/components/auth/RegisterForm.tsx
+++ frontend/components/auth/RegisterForm.tsx
@@ -1,39 +1,78 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FaApple, FaGithub, FaGoogle } from "react-icons/fa";
+
+interface InputFieldProps {
+  label: string;
+  type: string;
+  id: string;
+}
+
+const InputField = ({ label, id, type }: InputFieldProps): React.ReactNode => {
+  return (
+    <div>
+      <label htmlFor={id}>{label}</label> <br />
+      <input
+        type={type}
+        className="w-full mt-2 rounded-sm border border-gray-700 px-2 py-2 font-extralight"
+        placeholder={label}
+      />
+    </div>
+  );
+};
 
 const RegisterForm = () => {
   return (
-    <div className='flex'>
-      <div className='flex-1'>
-        <h1 className='text-white'>IMAGE</h1>
-      </div>
-      <div className='flex-1 flex-col text-white'>
-        <div>
-          <h1>Create an account</h1>
-          <p>Let's get started. Fill in the details below to create your account</p>
+    <div className="font-roboto flex h-screen items-center ">
+      <div className="flex-1 flex justify-center px-5 bg-neutral-900 items-center h-screen">
+        <div className="bg-blue-200 h-[35%] w-full">
+          IMAGE
         </div>
-        <form action="">
+      </div>
+      <div className="flex-1/4 flex justify-center h-full text-white">
+        <div className="flex flex-col gap-5 justify-center h-full w-100">
           <div>
-            <label htmlFor="name">Name</label>
-            <input id="name" />
+            <h1 className="text-4xl font-medium">Create an account</h1>
           </div>
           <div>
-            <label htmlFor="email">Email</label>
-            <input id='email' />
+            <p className="font-light">
+              Let's get started. Fill in the details below to create your account
+            </p>
           </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input id='password' type='password' />
-          </div>
-          <button className='bg-white cursor-pointer text-black'><Link href={``} >Sign Up</Link></button>
-        </form>
-        <div>
-          <img src={`${null}`} alt="" />
+          <form action="" className="flex flex-col gap-5">
+            <InputField label="Name" id="name" type="text" />
+            <InputField label="Password" id="password" type="password" />
+            <InputField label="Email" id="email" type="email" />
+
+            <div className="flex justify-center">
+              <button className="cursor-pointer w-full p-2 rounded-md bg-white text-black">
+                <Link href={``}><span className="">Sign Up</span></Link>
+              </button>
+            </div>
+            <div className="flex items-center my-4">
+              <div className="flex-grow h-px bg-gray-800" />
+              <span className="mx-4 text-sm text-gray-400">OR SIGN IN WITH</span>
+              <div className="flex-grow h-px bg-gray-800" />
+            </div>
+            <div className="flex justify-center">
+              <div className="flex space-x-4 w-full">
+                <button className="p-4 flex-1 flex justify-center border rounded-xl cursor-pointer text-white border-gray-700">
+                  <FaGithub size={20} />
+                </button>
+                <button className="p-4 flex-1 flex justify-center border rounded-xl cursor-pointer text-white border-gray-700">
+                  <FaGoogle size={20} />
+                </button>
+                <button className="p-4 flex-1 flex justify-center border rounded-xl cursor-pointer text-white border-gray-700">
+                  <FaApple size={20} />
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
