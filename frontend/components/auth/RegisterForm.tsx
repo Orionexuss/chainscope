5a180fcd66f044d44b64ee759ad8257a23b29926
Orionexuss <sebastianvargaslopez36@gmail.com>
2025-04-30 13:50:11 -0500

    feat: improve UI and add smooth scroll behavior in the register form

--- frontend/components/auth/RegisterForm.tsx
+++ frontend/components/auth/RegisterForm.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { FaApple, FaGithub, FaGoogle } from "react-icons/fa";
 
 interface InputFieldProps {
@@ -27,14 +28,14 @@ const InputField: React.FC<InputFieldProps> = ({ label, id, type }) => (
 
 const CreatorText: React.FC = () => (
   <div className="hidden h-full flex-1 items-center justify-center bg-neutral-950 px-5 sm:flex">
-    <div className="flex h-full w-full flex-col gap-7 rounded-2xl px-5 py-3 text-white">
+    <div className="flex h-full w-full flex-col gap-7 rounded-2xl px-12 py-3 text-white">
       <div className="h-18 w-18 mt-auto overflow-hidden rounded-full">
         <Image
           src="/Me.jpg"
           className="h-full w-full object-fill"
           alt="creator"
-          width={50}
-          height={50}
+          width={80}
+          height={80}
         />
       </div>
       <p className="z-10 clear-left font-light">
@@ -54,25 +55,36 @@ const socialButtonStyle =
   "flex flex-1 cursor-pointer justify-center rounded-xl border border-gray-700 p-4 text-white";
 
 const RegisterForm: React.FC = () => {
+  const formRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (formRef.current) {
+      formRef.current.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  }, []);
+
   return (
     <div className="font-roboto relative flex h-screen w-screen items-center overflow-hidden">
       {/* Background blobs */}
       <div className="bg-primary pointer-events-none absolute left-[35%] top-[10%] h-[35rem] w-[40rem] rounded-full opacity-50 blur-[8rem] filter" />
-      <div className="pointer-events-none absolute bottom-[5%] left-[60%] h-[35rem] w-[40rem] rounded-full bg-[#441578] opacity-90 blur-[8rem] filter" />
+      <div className="pointer-events-none absolute bottom-[5%] left-[60%] h-[35rem] w-[40rem] rounded-full bg-[#7c3fbe] opacity-60 blur-[8rem] filter" />
 
       <CreatorText />
 
-      <div className="flex-1/4 py-15 z-10 flex h-screen justify-center text-white sm:h-fit sm:py-0">
-        <div className="z-1 flex h-full flex-col justify-center gap-5 rounded-2xl border border-[#E0E0E0] bg-[rgba(0,0,0,0.6)] p-8 px-4 shadow-[0_4px_15px_rgba(198,198,198,0.5)] sm:w-[80%] sm:p-5">
+      <div className="flex-1/4 z-10 flex h-full items-center justify-center overflow-auto py-10 text-white">
+        <div
+          className="sm:px-13 flex min-h-fit flex-col gap-3 rounded-2xl border border-[#E0E0E0] bg-[rgba(0,0,0,0.6)] p-8 px-4 shadow-[0_4px_15px_rgba(198,198,198,0.5)] sm:w-[70%]"
+          ref={formRef}
+        >
           <div className="flex justify-center">
             <h1 className="text-4xl font-medium">Create an account</h1>
           </div>
 
-          <p className="font-light">
+          <p className="max-w-[80%] font-light">
             Let's get started. Fill in the details below to create your account.
           </p>
 
-          <form className="flex flex-col gap-5">
+          <form className="flex flex-col gap-3">
             <InputField label="Name" id="name" type="text" />
             <InputField label="Password" id="password" type="password" />
             <InputField label="Email" id="email" type="email" />
@@ -106,6 +118,14 @@ const RegisterForm: React.FC = () => {
               <button type="button" className={socialButtonStyle}>
                 <FaApple size={20} />
               </button>
+            </div>
+            <div className="flex justify-center">
+              <span>
+                Already have an account?{" "}
+                <a href="" className="underline">
+                  Sign in
+                </a>
+              </span>
             </div>
           </form>
         </div>
