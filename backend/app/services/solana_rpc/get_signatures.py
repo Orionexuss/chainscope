1371885d729c609d717c21f01f7bf080966a319e
Orionexuss <sebastianvargaslopez36@gmail.com>
2025-05-16 23:49:43 -0500

    feat: add get_signatures rpc method

---
+++ backend/app/services/solana_rpc/get_signatures.py
@@ -0,0 +1,20 @@
+from solana.rpc.api import Client
+from solana.rpc.commitment import Commitment
+from solders.pubkey import Pubkey
+
+
+def get_signatures(
+    pubkey: str,
+    client_url: str = "wss://api.devnet.solana.com",
+):
+    solana_client = Client(client_url)
+    all_signatures = []
+
+    while True:
+        response = solana_client.get_signatures_for_address(
+            Pubkey.from_string(pubkey), limit=None, commitment=Commitment("finalized")
+        )
+        all_signatures.extend([str(tx.signature) for tx in response.value])
+        break
+
+    return all_signatures
